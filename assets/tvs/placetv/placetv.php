7ad2add0797d87333e36b406dd7ef8d4927e6f3b
Yurik <yurik@unix.am>
2013-04-26 17:53:58 +0400

    First commit

---
+++ assets/tvs/placetv/placetv.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ PlaceTV — custom TV for MODx Evolution
+ Version 13.04.16 by yurik@unix.am
+*/
+
+if (IN_MANAGER_MODE != 'true') {
+ die('<h1>ERROR:</h1><p>Please use the MODx Content Manager instead of accessing this file directly.</p>');
+}
+
+$includeOnce = <<<EOD
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script src="/assets/js/maps.js" type="text/javascript"></script>
+EOD;
+
+if(!defined('PLACETV')) {
+ echo $includeOnce;
+ define('PLACETV', 1);
+}
+
+$value = empty($row['value']) ? $row['default_text'] : $row['value'];
+$id = $row['id'];
+
+$input = <<<EOD
+<input type="text" id="tv{$id}" name="tv{$id}" value="{$value}" style="width:300px;" onchange="documentDirty=true;"/><input id="tv{$id}geocode" type="button" style="vertical-align:top;" value="Найти"/>
+<div class="map" id="map{$id}" style="height:300px; background:#eee; width:100%;"></div>
+<script type="text/javascript">
+var map{$id} = new Maps();
+var point{$id} = new google.maps.LatLng({$value});
+map{$id}.createMap('map{$id}', point{$id}, 16);
+var mapMarker{$id} = map{$id}.createDraggableMarker(point{$id}, 'Местоположение', 'tv{$id}');
+$('tv{$id}geocode').addEvents({
+ 'click':function() {
+  var value = $('tv{$id}').value;
+  map{$id}.geocodeMarker(mapMarker{$id}, value);
+ }
+});
+</script>
+EOD;
+
+echo $input;
+?>
